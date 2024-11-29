832336256652adad94c3c501ff7ba0f3afbf9ada
AndreaDiazZagal <20213tn002@utez.edu.mx>
2024-11-29 16:46:15 -0600

    uso de indexDB

--- server.js
+++ server.js
@@ -11,6 +11,12 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'public/index.html'))
 })
 
+app.get('/api/data', (req, res) => {
+  // Este endpoint puede devolver datos de la base de datos
+  res.json({ message: 'Prueba de mensaje para PouchDB Emiliano Ortiz' });
+});
+
 app.listen(httpPort, function () {
   console.log(`Listening on port ${httpPort}!`)
 })
+
